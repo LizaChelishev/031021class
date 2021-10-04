01df043ae2903e5985ca6c1792fc06fe91344a30
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-04 11:15:01 +0300

    Create task4perek10.py

---
+++ task4perek10.py
@@ -0,0 +1,27 @@
+# 170 countires are voting pro / con / abst / vetto. count the votes and break look is vetto. 
+num_of_countries = 0
+count_pro = 0
+count_con = 0
+count_abst = 0
+while True:
+vote = int(input('Input your vote')) #( 1 / 2 / 3 / 4 )
+    if vote = 1:
+        vote = pro
+        pro_count =+ 1
+    if vote = 2:
+        vote = con 
+        con_count =+1
+    if vote = 3:
+        vote = abst 
+        con_abst =+1    
+    if vote = 4:
+        vote = vetto
+        print('Country number' str(num_of_countries) 'voted vetto')
+        break  
+    num_of_countries = num_of_countries + 1
+  
+
+if num_of_countries == 170:
+    print('Pro votes = str(pro_count)')
+    print('Con votes = str(con_count)')
+    print('Abstention votes = str(abst_count)')
