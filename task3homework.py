8f966fde8207ad5627ad12868aaac2ad995a37e2
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-04 15:49:23 +0300

    Create task3homework.py

---
+++ task3homework.py
@@ -0,0 +1,23 @@
+while True: 
+  number = int(input('Enter a number'))
+  if number % 2 ==0:
+    number = int(input('Enter a number'))
+    if number % 3 == 0: 
+      number = int(input('Enter a number'))
+      if number % 4 == 0: 
+        number = int(input('Enter a number'))
+        if number % 5 == 0: 
+          number = int(input('Enter a number'))
+          if number % 6 == 0:
+            number = int(input('Enter a number'))
+            if number % 7 == 0:
+              number = int(input('Enter a number'))
+              if number % 8 == 0:
+                number = int(input('Enter a number'))
+                if number % 9 == 0:
+                  number = int(input('Enter a number'))
+                  if number % 10 == 0
+                  break
+ if number < 0:
+  continue
+ 
