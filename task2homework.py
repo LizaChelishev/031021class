c9368726e203d5487b306f90dc80a8f5a14137e6
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-04 17:21:09 +0300

    Create task2homework.py

---
+++ task2homework.py
@@ -0,0 +1,11 @@
+number1 = int(input('Enter a number'))
+sum = 0 
+numbers_bigger_than_77 = 0
+while number > 77: 
+  number2 = int(input('Enter a number'))
+  sum = number1 + number2
+  number1= number 2
+  numbers_bigger_than_77 =+ 1
+  
+if number2 == 0 :
+  break
